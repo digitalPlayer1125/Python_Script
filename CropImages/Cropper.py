403c3673a463a5312005157ed88f638920af6d4e
Rishi Raj Jain <46300090+digitalPlayer1125@users.noreply.github.com>
2019-05-08 11:37:42 +0530

    Update Cropper.py

--- CropImages/Cropper.py
+++ CropImages/Cropper.py
@@ -7,6 +7,14 @@ def crop(image_path, coords, saved_location):
     cropped_image.show()
 
 if __name__ == '__main__':
+    print("Input the x of the origin")
+    x1= int(input())
+    print("Input the y of the origin")
+    y1= int(input())
+    print("Input the x of the end")
+    x2= int(input())
+    print("Input the y of the end")
+    y2= int(input())
     for i in range(1, j): #Set j as the number of images with the number of images with the same name
         image = 'Name_Of_Your_Image(%s).png' %i
-        crop(image, (0, 180, 1000, 620), 'Final_Name_Of_The_Image(%s).png'%i)
+        crop(image, (x1, y1, x2, y2), 'Final_Name_Of_The_Image(%s).png'%i)
